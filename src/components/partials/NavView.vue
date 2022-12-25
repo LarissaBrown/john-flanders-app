60ab1157134181da222e5cfe4652318c3dd4b06a
Larissa Brown <larisa.brown.johnson@gmail.com>
2022-12-25 11:42:57 -0700

    comment out import of AboutView in NavView component

---
+++ src/components/partials/NavView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <nav class="navbar container" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="jflogo" href="/">
+          <img alt="logo John Flanders" src="../../assets/logo.png">
+        </a>
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbar" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link to="/" class="navbar-item">Home</router-link>
+          <router-link to="/about" class="navbar-item">About</router-link>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-dark">
+                <strong>Sign In</strong>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    </div>
+  </template>
+  <script>
+//import AboutView from '@/views/AboutView.vue';
+
+    export default {
+      name: 'NavView',
+
+    };
+  </script>
+  <style lang="scss" scoped>
+    nav {
+      padding-top: 25px;
+      padding-bottom: 30px;
+      padding-left: 25px;
+      a {
+        font-weight: bold;
+        color: #2c3e50;
+        &.router-link-exact-active {
+          color: #ca8608;
+        }
+      }
+    }
+    .jflogo {
+      height: 100%;
+    }
+  </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="hero" id="egyptian">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-size-1">About John Flanders</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <p class="org-description is-size-4">
+          John Flanders has been one of the most in demand woodwind players in Salt Lake City and the Intermountain West for over a decade. You can hear him on t.v. and radio commercials, as well as television shows and movie soundtracks. 
+      </p>
+      <p class="org-description is-size-4">
+        In addition to being a renowned bandleader and session player, John’s recordings have received critical and popular acclaim, leading to a growing reputation as a composer and arranger. 
+      </p>
+      <p class="org-description is-size-4">
+        John has won numerous prestigious City Weekly Best of Utah awards as an instrumentalist as well as with his classic jazz groups. He has performed the National Anthem solo at NBA games, and toured Italy, France, Germany, Switzerland. He also has performed with the Utah Symphony on multiple occasions. 
+      </p> 
+      <p class="org-description is-size-4"> 
+        John and his group "Double Helix" currently have three high energy jazz/swing/funk/latin cds out, as well as solo cds from Flanders. CDs and performance information is on the website johnflanders.com.
+      </p>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.title {
+  color: rgb(76, 76, 242);
+}
+  .org-description {
+    margin-top: 50px;
+  }
+  #egyptian {
+    background-image: url("../assets/EgyptianTheatreJohnFlanders.jpeg" )
+  }
+</style>
