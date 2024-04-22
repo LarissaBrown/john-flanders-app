0e8fa0ac7a6411b3347c5859233cbd447037cebe
Lolly <larissabrowndeveloper@gmail.com>
2024-04-22 16:22:02 -0600

    pullrequests: format changes AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,16 +9,22 @@
     </div>
     <div class="container">
       <p class="org-description is-size-4">
-          John Flanders has been one of the most in demand woodwind players in Salt Lake City and the Intermountain West for over a decade. You can hear him on t.v. and radio commercials, as well as television shows and movie soundtracks. 
+        John Flanders has been one of the most in demand woodwind players in Salt Lake City and the Intermountain West
+        for over a decade. You can hear him on t.v. and radio commercials, as well as television shows and movie
+        soundtracks.
       </p>
       <p class="org-description is-size-4">
-        In addition to being a renowned bandleader and session player, John’s recordings have received critical and popular acclaim, leading to a growing reputation as a composer and arranger. 
+        In addition to being a renowned bandleader and session player, John’s recordings have received critical and
+        popular acclaim, leading to a growing reputation as a composer and arranger.
       </p>
       <p class="org-description is-size-4">
-        John has won numerous prestigious City Weekly Best of Utah awards as an instrumentalist as well as with his classic jazz groups. He has performed the National Anthem solo at NBA games, and toured Italy, France, Germany, Switzerland. He also has performed with the Utah Symphony on multiple occasions. 
-      </p> 
-      <p class="org-description is-size-4"> 
-        John and his group "Double Helix" currently have three high energy jazz/swing/funk/latin cds out, as well as solo cds from Flanders. CDs and performance information is on the website johnflanders.com.
+        John has won numerous prestigious City Weekly Best of Utah awards as an instrumentalist as well as with his
+        classic jazz groups. He has performed the National Anthem solo at NBA games, and toured Italy, France, Germany,
+        Switzerland. He also has performed with the Utah Symphony on multiple occasions.
+      </p>
+      <p class="org-description is-size-4">
+        John and his group "Double Helix" currently have three high energy jazz/swing/funk/latin cds out, as well as
+        solo cds from Flanders. CDs and performance information is on the website johnflanders.com.
       </p>
     </div>
   </div>
@@ -27,11 +33,12 @@
 .title {
   color: rgb(76, 76, 242);
 }
-  .org-description {
-    margin-top: 50px;
-  }
-  #egyptian {
-    background-image: url("../assets/EgyptianTheatreJohnFlanders.jpeg" )
-  }
-</style>
 
+.org-description {
+  margin-top: 50px;
+}
+
+#egyptian {
+  background-image: url("../assets/EgyptianTheatreJohnFlanders.jpeg")
+}
+</style>
